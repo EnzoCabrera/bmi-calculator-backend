e16a76fe110bdb1ad0cc8b2304cb9f94a45d8253
GS-COOMPUTER\Geziel <geziel05@gmail.com>
2025-04-15 18:01:18 -0300

    Chore: Added openapi.py so Swagger now has bearer token authentication available

---
+++ bmi-calculator/backend/app/api/openapi.py
@@ -0,0 +1,24 @@
+from fastapi.openapi.utils import get_openapi
+from fastapi import FastAPI
+
+def custom_openapi(app: FastAPI):
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="BMI Calculator",
+        version="1.0.0",
+        routes=app.routes,
+    )
+    openapi_schema["components"]["securitySchemes"] = {
+        "BearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",
+        }
+    }
+    for path in openapi_schema["paths"].values():
+        for method in path.values():
+            method.setdefault("security", []).append({"BearerAuth": []})
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
