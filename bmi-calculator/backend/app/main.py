58c585dfc0b6dd6f9b260eec493f9f60f12773fa
EnzoCabrera <enzo2005cabrera@gmail.com>
2025-05-16 20:07:51 -0300

    Chore: Changing CORS URL

--- bmi-calculator/backend/app/main.py
+++ bmi-calculator/backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["https://bmifit.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
