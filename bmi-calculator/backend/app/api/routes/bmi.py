ae56a77bc0d2ed106022c2a2a6e8d8252d4f3b7b
Enzo Nascimento Cabrera <enzo2005cabrera@gmail.com>
2025-05-19 17:24:16 -0300

    Feat: Creating GET endpoints for BMI

--- bmi-calculator/backend/app/api/routes/bmi.py
+++ bmi-calculator/backend/app/api/routes/bmi.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import Session
 from app.api.auth import get_current_user
 from app.db.session import get_db
 from app.services.bmi_service import calculate_bmi
-from app.db.models import User
-from app.services.endpoint_limit_service import check_bmi_limit
+from app.db.models import User, UserBMI
+from app.services.endpoint_limit_service import check_bmi_limit, get_rate_limiter, post_rate_limiter
 from datetime import datetime
 
 router = APIRouter()
@@ -15,8 +15,8 @@ class CalculateBMI(BaseModel):
     height: float
 
 # Calculating the user's BMI and saving it to the DB
-@router.put("/bmi")
-def get_bmi(data: CalculateBMI, db: Session = Depends(get_db), user: User = Depends(get_current_user), _: None = Depends(check_bmi_limit)):
+@router.put("/create", dependencies=[Depends(post_rate_limiter)])
+def create_bmi(data: CalculateBMI, db: Session = Depends(get_db), user: User = Depends(get_current_user), _: None = Depends(check_bmi_limit)):
     if data.height <= 0:
         raise HTTPException(status_code=400, detail="Height must be greater than zero")
     if data.weight <= 0:
@@ -31,3 +31,14 @@ def get_bmi(data: CalculateBMI, db: Session = Depends(get_db), user: User = Depe
         }
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
+# Endpoint to get user's bmi
+@router.get("/latest-by-id", dependencies=[Depends(get_rate_limiter)])
+def get_bmi(db: Session = Depends(get_db), user: User = Depends(get_current_user)):
+        user_bmi: UserBMI = (db.query(UserBMI).filter(UserBMI.user_id == user.id).order_by(UserBMI.created_at.desc()).first())
+
+        if not user_bmi:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        return user_bmi
